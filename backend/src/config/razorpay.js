eb5dbffcce2788a06e7cb94681a4926f277cccb2
mdfaizan0 <faizanm78692@gmail.com>
2026-02-27 16:27:55 +0530

    fix: deployment error fix

--- backend/src/config/razorpay.js
+++ backend/src/config/razorpay.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv"
+import Razorpay from "razorpay"
 
 dotenv.config()
 
---
+++ backend/src/controllers/webhook.controller.js
@@ -0,0 +1,8 @@
+export async function handleRazorpayWebhook(req, res) {
+    try {
+        // TODO: Implement webhook handler
+
+    } catch (error) {
+        console.log(error)
+    }
+}
